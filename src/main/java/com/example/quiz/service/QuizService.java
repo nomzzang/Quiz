9c859208cc83c454561037e8192593e6e5764364
kyuzzang <starnomzzang@naver.com>
2023-04-08 18:37:50 +0900

    final

---
+++ src/main/java/com/example/quiz/service/QuizService.java
@@ -0,0 +1,32 @@
+package com.example.quiz.service;
+
+import com.example.quiz.entity.Quiz;
+
+import java.util.Optional;
+
+
+// Quiz 서비스 : service
+public interface QuizService {
+
+    // 등록된 모든 퀴즈 정보를 가져옵니다.
+    Iterable<Quiz> selectAll();
+
+    // id를 키로 사용해 퀴즈 정보를 한건 가져옵니다.
+    Optional<Quiz> selectOneById(Integer id);
+
+    // 퀴즈 정보를 랜덤으로 한건 가져옵니다.
+    Optional<Quiz> selectOneRandomQuiz();
+
+    // 퀴즈의 정답/오답 여부를 판단합니다.
+    Boolean checkQuiz(Integer id, boolean myAnswer);
+
+    // 퀴즈를 등록합니다.
+    void insertQuiz(Quiz quiz);
+
+    // 퀴즈를 변경합니다.
+    void updateQuiz(Quiz quiz);
+
+    // 퀴즈를 삭제합니다.
+    void deleteQuizById(Integer id);
+
+}
